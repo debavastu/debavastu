7164ddd62526ea9d0e1eb34d3cc77daee8e905ca
debavastu <debavastu@gmail.com>
2025-11-17 21:49:31 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,9 @@
+const CACHE = 'debavastu-v1';
+const ASSETS = ['/', '/index.html', '/manifest.json', '/img/logo.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
