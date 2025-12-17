eda6d0447f42d8f60fd82212e241707576ec7697
debavastu <debavastu@gmail.com>
2025-12-17 18:16:47 +0530

    Create main.bundle.js

---
+++ js/main.bundle.js
@@ -0,0 +1,51 @@
+/*! DebaVastu JS Bundle | Production */
+
+////////////////////
+// jQuery Core
+////////////////////
+/* jquery.min.js */
+/* paste full jquery.min.js here */
+
+////////////////////
+// jQuery Migrate
+////////////////////
+/* jquery-migrate-3.0.0.js */
+/* paste full file here */
+
+////////////////////
+// Bootstrap
+////////////////////
+/* bootstrap.min.js */
+/* paste full file here */
+
+////////////////////
+// Owl Carousel
+////////////////////
+/* owl-carousel.js */
+/* paste full file here */
+
+////////////////////
+// WOW Animations
+////////////////////
+/* wow.min.js */
+/* paste full file here */
+
+////////////////////
+// CounterUp + Waypoints
+////////////////////
+/* waypoints.min.js */
+/* paste full file here */
+/* jquery.counterup.min.js */
+/* paste full file here */
+
+////////////////////
+// Magnific Popup
+////////////////////
+/* jquery.magnific-popup.min.js */
+/* paste full file here */
+
+////////////////////
+// Mediplus Main
+////////////////////
+/* main.js */
+/* paste full file here */
